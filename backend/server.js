451f491333020835075525220b177fb8446bd788
siri746 <23b01a4510@svecw.edu.in>
2026-02-28 16:33:56 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
 const urlRoutes = require("./routes/urlRoutes");
 
 const app = express();
 
-// middleware
-app.use(cors());
+// VERY IMPORTANT
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
-// connect database
 connectDB();
 
-// routes
 app.use("/api/url", urlRoutes);
 
-// root route
 app.get("/", (req, res) => {
   res.send("URL Shortener API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
